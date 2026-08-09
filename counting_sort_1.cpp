844e25a5c6af5f063d61d6e890c435f1884529f0
SHAIK MOHAMMAD SYED GOUSH <shaikmohammadsyedgoush@gmail.com>
2026-08-09 21:34:47 +0530

    Create counting_sort_1.cpp
    Add Counting Sort 1 solution

---
+++ counting_sort_1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int freq[100] = {0};
+
+    for(int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        freq[x]++;
+    }
+
+    for(int i = 0; i < 100; i++)
+    {
+        cout << freq[i] << " ";
+    }
+
+    return 0;
+}
